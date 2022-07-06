ab6c3e0fdbcd8b947bc911fb010213041a41e4b4
Alexandre Silva <42013051+alexandre-87mc@users.noreply.github.com>
2022-07-06 09:08:25 -0300

    f

--- Data_Frames_Operations_pandas.py
+++ Data_Frames_Operations_pandas.py
@@ -24,7 +24,49 @@ print('\n')
 print(df[(df['col1']>2) & (df['col2']>500)])
 print('\n')
 
-def times2():
+#Using apply
+def times2(x):
     return x*2
+print(df['col1'].apply(times2))
+print('\n')
 
+#Delete column
+#del df['col2'] #To use it again remove '#'
+print(df)
+print('\n')
+
+#Values of columns or index
+print(df.columns)
+print(df.index)
+print('\n')
+
+#Order values
+#df.sort_values(by='col2', inplace=True) #To use it again remove '#'
+print(df)
+print('\n')
+
+#Check if df is null
+df.isnull
+print(df)
+print('\n')
+
+#Erase Nam values
+df.dropna()
+print(df)
+print('\n')
+
+#Fill values to df
+print(df['col1'].fillna(value=df['col1'].mean()))
+
+#Pivot tables
+data = {'A':['foo', 'foo', 'foo', 'bar', 'bar', 'bar'],
+        'B':['one', 'one', 'two', 'two', 'one', 'one'],
+        'C':['x', 'y', 'x', 'y', 'x', 'y'],
+        'D':[1,3,2,5,4,1]}
+
+df_PVT = pd.DataFrame(data)
+print(df_PVT)
+print('\n')
+
+print(df_PVT.pivot_table(values='D', index=['A','B'], columns='C'))
 
