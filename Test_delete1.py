7f0cf9ca4729f9a4d002d1234a6fb24c42613379
Alexandre Silva <42013051+alexandre-87mc@users.noreply.github.com>
2022-07-13 08:35:49 -0300

    m

---
+++ Test_delete1.py
@@ -0,0 +1,14 @@
+def calPoints(ops) -> int:
+    """
+    :type ops: List[str] - List of operations
+    :rtype: int - Sum of scores after performing all operations
+    """
+    result = None
+
+    return result
+
+if __name__=='__main__':
+    line = input()
+    ops = line.strip().split()
+
+    print(calPoints(ops))
---
+++ Test_delete2.py
@@ -0,0 +1,18 @@
+from numpy import True_
+
+
+def isValid(s: str) -> bool:
+    """
+    :type s: str - String to be tested for validity
+    :rtype: bool - returns true if the string is valid else false
+    """
+    result = None
+
+    return result
+
+if __name__ == "__main__":
+    line = input()
+    if isValid(line):
+        print("valid")
+    else:
+        print("invalid")
